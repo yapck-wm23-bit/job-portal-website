3529d6ad63e5cf91cffdd4153beec61b87370335
ngpx-wm23 <ngpx-wm23@student.tarc.edu.my>
2026-07-13 09:43:00 +0800

    JPW-3 Add salary range validation to job posting form

--- config/database.php
+++ config/database.php
@@ -1,1 +1,24 @@
-#For job posting form
+<?php
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "job_portal_db";
+
+$conn = new mysqli(
+    $host,
+    $username,
+    $password,
+    $database
+);
+
+if ($conn->connect_error) {
+    die(
+        "Database connection failed: "
+        . $conn->connect_error
+    );
+}
+
+$conn->set_charset("utf8mb4");
+
+?>
